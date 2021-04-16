3c40b305325b88bff8f2acc2e10ae12430911891
Prasan Nimantha <nimoprasan@gmail.com>
2021-04-17 00:08:52 +0530

    created our models and refactored using project lombok

---
+++ src/main/java/com/prashan/springit/model/Link.java
@@ -0,0 +1,20 @@
+package com.prashan.springit.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String tittle;
+    private String url;
+}
