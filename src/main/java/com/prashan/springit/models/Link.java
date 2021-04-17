683b03ac68982d5524ad2c45d206b9a78aa70308
Prasan Nimantha <nimoprasan@gmail.com>
2021-04-17 14:44:04 +0530

    Added mappings for Link to comment

--- src/main/java/com/prashan/springit/models/Link.java
+++ src/main/java/com/prashan/springit/models/Link.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,4 +19,7 @@ public class Link {
     private Long id;
     private String tittle;
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
