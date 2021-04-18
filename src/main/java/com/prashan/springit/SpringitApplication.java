a5d07dee47f06f3d562770b017027831f797b765
Prasan Nimantha <nimoprasan@gmail.com>
2021-04-18 21:44:54 +0530

    commented out commandline runner

--- src/main/java/com/prashan/springit/SpringitApplication.java
+++ src/main/java/com/prashan/springit/SpringitApplication.java
@@ -24,7 +24,7 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	@Bean
+	//@Bean
 	CommandLineRunner commandLineRunner (LinkRepository linkRepository, CommentRepository commentRepository){
 		return args -> {
 			Link link = new Link("Getting Started with Spring Boot 2", "https://therealdanvega.com/spring-boot-2");
